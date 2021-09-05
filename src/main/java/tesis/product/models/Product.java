6015dc8ab0428c51a3ef8ca32a9a044b9226865f
Enzo Roselli <48778578+EnzoRoselli@users.noreply.github.com>
2021-09-04 21:33:35 -0300

    Initial commit

---
+++ src/main/java/tesis/product/models/Product.java
@@ -0,0 +1,35 @@
+package tesis.product.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "image")
+    private String image;
+    @Column(name = "clasification")
+    private String clasification;
+    @Column(name = "description")
+    private String description;
+}
---
+++ src/main/java/tesis/product/utils/ParametersDefaultValue.java
@@ -0,0 +1,5 @@
+package tesis.product.utils;
+
+public class ParametersDefaultValue {
+    public static final String CLASIFICATIONS = "Cuidado Personal,Limpieza,Bebidas,Almacen";
+}
