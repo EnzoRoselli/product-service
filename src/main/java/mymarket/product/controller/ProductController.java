30a42a27c06194e26f7dff771af46bdd8316ae9a
MateuGit <facundomateu@hotmail.com>
2021-11-19 20:59:26 -0300

    status codes fix

--- src/main/java/mymarket/product/controller/ProductController.java
+++ src/main/java/mymarket/product/controller/ProductController.java
@@ -6,9 +6,20 @@ import lombok.extern.slf4j.Slf4j;
 import mymarket.product.commons.models.Product;
 import mymarket.product.commons.models.enums.Clasifications;
 import mymarket.product.service.ProductService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import static mymarket.product.commons.utils.ParametersDefaultValue.CLASIFICATIONS;
 
@@ -22,8 +33,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public Product save(@RequestBody Product product) {
-        return productService.save(product);
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        return ResponseEntity.created(getLocation(productService.save(product))).build();
     }
 
     @DeleteMapping("{id}")
@@ -37,8 +48,20 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<Product> getByClasificationsAndName(@RequestParam(required = false, defaultValue = CLASIFICATIONS) List<Clasifications> clasifications,
-                                                    @RequestParam(required = false, defaultValue = "") String name) {
-        return productService.getByClasificationsAndName(clasifications, name);
+    public ResponseEntity<List<Product>> getByClasificationsAndName(@RequestParam(required = false, defaultValue = CLASIFICATIONS) List<Clasifications> clasifications,
+                                                                    @RequestParam(required = false, defaultValue = "") String name) {
+        return Optional.of(productService.getByClasificationsAndName(clasifications, name))
+                .filter(List::isEmpty)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.noContent().build());
+    }
+
+    private URI getLocation(Product product) {
+
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(product.getId())
+                .toUri();
     }
 }
